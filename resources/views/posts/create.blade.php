7d32c59c835ae130a5608c23db42161fd343b0dd
deniskhen <denis.khen79@mail.ru>
2023-08-26 19:49:18 +0300

    сделано create&store

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -6,16 +6,19 @@
         @csrf
         <div class="mb-3">
         <label for="title" class="form-label">
+            Title:
             <input type="text" name="title" id="title" class="form-control" required>
         </label>
         </div>
         <div class="mb-3">
         <label for="content" class="form-label">
+            Content:
             <textarea name="content" id="content" cols="30" rows="10" class="form-control" required></textarea>
         </label>
         </div>
         <div class="mb-3">
         <label for="description" class="form-label">
+            Description:
             <textarea name="description" id="description" cols="30" rows="3" class="form-control" required></textarea>
         </label>
         </div>
